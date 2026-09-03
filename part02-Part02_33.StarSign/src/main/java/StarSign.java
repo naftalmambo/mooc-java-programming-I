c4ccb1ce25324b95b82379906d560299b8d060ae
Naftal <naftalmmb@gmail.com>
2026-09-03 22:05:44 +0300

    Organse files for better workflow

---
+++ part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -0,0 +1,71 @@
+
+public class StarSign {
+
+    public static void main(String[] args) {
+
+        // The tests are not checking the main, so you can modify it freely.
+        // NB: If the tests don't seem to pass, you should try the methods here
+        // in the main to make sure they print the correct shapes!
+
+        printStars(5);
+        printStars(3);
+        printStars(9);
+
+        printSquare(4);
+
+        printRectangle(17, 3);
+
+        printTriangle(4);
+    }
+
+    public static void printStars(int number) {
+        // first part of the exercise
+        int i = 1;
+        while (i <= number) {
+            System.out.print("*");
+            i++;
+        }
+        System.out.println("");
+
+    }
+
+    public static void printSquare(int size) {
+        // second part of the exercise
+
+        int row = 1;
+
+        while (row <= size) {
+
+            printStars(size);
+
+            row++;
+
+        }
+
+    }
+
+    public static void printRectangle(int width, int height) {
+        // third part of the exercise
+
+        int row = 1;
+
+        while (row <= height) {
+            printStars(width);
+            row++;
+
+        }
+
+    }
+
+    public static void printTriangle(int size) {
+        // fourth part of the exercise
+
+        int row = 1;
+        while (row <= size) {
+            printStars(row);
+            row++;
+
+        }
+
+    }
+}
