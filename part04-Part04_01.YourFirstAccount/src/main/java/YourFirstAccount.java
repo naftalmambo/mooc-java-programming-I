868a09f5e45b7aa3649fdbd4af4489ae26d5a512
Naftal <naftalmmb@gmail.com>
2026-09-03 12:05:26 +0300

    Unpack part-04 from the folder

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,11 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+
+        Account naftalAccount = new Account("Naftal's account", 100.00);
+
+        naftalAccount.deposit(20.00);
+
+        System.out.println(naftalAccount);
     }
 }
