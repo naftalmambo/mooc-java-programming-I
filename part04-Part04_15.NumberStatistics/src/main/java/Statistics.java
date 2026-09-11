bc697776654c69695882f45ba5adfdc29fc311a3
Naftal <naftalmmb@gmail.com>
2026-09-11 16:25:55 +0300

    feat(mooc): complete part 04 exercise 15

--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,39 @@
-
 public class Statistics {
+    private int count;
+    private int sum;
+
+    public Statistics() {
+        // initialize the variable numberCount here
+
+        this.count = 0;
+        this.sum = 0;
+
+    }
+
+    public void addNumber(int number) {
+        // write code here
+        this.sum = sum + number;
+
+        this.count = count + 1;
+
+    }
+
+    public int getCount() {
+        // write code here
+        return this.count;
+    }
+
+    public int sum() {
+
+        return this.sum;
+
+    }
+
+    public double average() {
+        if (this.count == 0) {
+            return 0.0;
+        }
+        return 1.0 * this.sum / this.count;
+
+    }
 }
