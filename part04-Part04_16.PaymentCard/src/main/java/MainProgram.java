1a23846833adf22786b38cadfae284640eef5858
Naftal <naftalmmb@gmail.com>
2026-09-15 10:34:07 +0300

    feat: complete payment card exercise

--- part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
+++ part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
@@ -3,9 +3,30 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Scanner scanner = new Scanner(System.in);
-        
+
         // Here you can write code to test that PaymentCard works as intended
         // be sure to erase the extra code for the last part of the exercise!
+        PaymentCard paulCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+
+        paulCard.eatHeartily();
+        mattsCard.eatAffordably();
+
+        System.out.println("Paul: " + paulCard);
+        System.out.println("Matt: " + mattsCard);
+
+        paulCard.addMoney(20);
+        mattsCard.eatHeartily();
+
+        System.out.println("Paul: " + paulCard);
+        System.out.println("Matt: " + mattsCard);
+
+        paulCard.eatAffordably();
+        paulCard.eatAffordably();
+
+        mattsCard.addMoney(50);
+        System.out.println("Paul: " + paulCard);
+        System.out.println("Matt: " + mattsCard);
 
     }
 }
