b3f70f987e7904cafdd8a5fcfeae70c85ad94c44
Naftal <naftalmmb@gmail.com>
2026-09-17 15:25:31 +0300

    feat: complete television programs exercise

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -9,5 +9,30 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+
+            }
+            System.out.print("Duration: ");
+            Integer duration = Integer.valueOf(scanner.nextLine());
+
+            programs.add(new TelevisionProgram(name, duration));
+
+        }
+        System.out.println();
+        System.out.print("Program's maximum duration? ");
+        Integer limit = Integer.valueOf(scanner.nextLine());
+
+        for (TelevisionProgram film : programs) {
+            if (film.getDuration() <= limit) {
+                System.out.println(film);
+
+            }
+
+        }
+
     }
 }
