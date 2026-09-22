00a4d7c9aefd9372dea72daa8262c8df5803b5fd
Naftal <naftalmmb@gmail.com>
2026-09-22 19:52:07 +0300

    complete Storing Records Exercise

--- part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -25,6 +25,24 @@ public class StoringRecords {
 
         // Write here the code for reading from file
         // and printing the read records
+
+        try (Scanner reader = new Scanner(Paths.get(file))) {
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+
+                Person newPerson = new Person(name, age);
+                persons.add(newPerson);
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
         return persons;
 
     }
