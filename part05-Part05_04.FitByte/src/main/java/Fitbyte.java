b5de3bdf2d8474094be261469624bbca7426ff59
Naftal <naftalmmb@gmail.com>
2026-09-23 19:43:43 +0300

    Implement Fitbyte class to complete the Exercise

---
+++ part05-Part05_04.FitByte/src/main/java/Fitbyte.java
@@ -0,0 +1,19 @@
+public class Fitbyte {
+
+    private int age;
+    private int restingHeartRate;
+
+    public Fitbyte(int age, int restingHeartRate) {
+        this.age = age;
+        this.restingHeartRate = restingHeartRate;
+
+    }
+
+    public double targetHeartRate(double percentageOfMaximum) {
+        double maxHeartRateRate = 206.3 - (0.711 * this.age);
+        double targetRate = (maxHeartRateRate - this.restingHeartRate) * percentageOfMaximum + this.restingHeartRate;
+        return targetRate;
+
+    }
+
+}
